c9badba1fd9c4db977d448fb1ed3c00ef25ebb0d
Karol Binkowski <karolbinkowski3@proton.me>
2026-03-10 11:33:44 +0100

    refactor: use Exceptions.MissingActivity

--- android/src/main/java/expo/modules/screenshotguard/ExpoScreenshotGuardExceptions.kt
+++
@@ -1,6 +0,0 @@
-package expo.modules.screenshotguard
-
-import expo.modules.kotlin.exception.CodedException
-
-internal class ActivityNotFoundException :
-  CodedException("Activity is not available")
--- android/src/main/java/expo/modules/screenshotguard/ExpoScreenshotGuardModule.kt
+++ android/src/main/java/expo/modules/screenshotguard/ExpoScreenshotGuardModule.kt
@@ -1,13 +1,14 @@
 package expo.modules.screenshotguard
 
 import android.view.WindowManager
+import expo.modules.kotlin.exception.Exceptions
 import expo.modules.kotlin.functions.Queues
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class ExpoScreenshotGuardModule : Module() {
   private val currentActivity
-    get() = appContext.currentActivity ?: throw ActivityNotFoundException()
+    get() = appContext.currentActivity ?: throw Exceptions.MissingActivity()
 
   override fun definition() = ModuleDefinition {
     Name("ExpoScreenshotGuard")

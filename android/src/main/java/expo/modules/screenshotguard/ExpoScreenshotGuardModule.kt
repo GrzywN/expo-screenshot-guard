16adf02381f43abe522b2b487ac6ec2480df4f70
Karol Binkowski <karolbinkowski3@proton.me>
2026-03-10 11:32:27 +0100

    fix: use runOnQueue(MAIN) and idiomatic currentActivity accessor

--- android/src/main/java/expo/modules/screenshotguard/ExpoScreenshotGuardModule.kt
+++ android/src/main/java/expo/modules/screenshotguard/ExpoScreenshotGuardModule.kt
@@ -1,32 +1,27 @@
 package expo.modules.screenshotguard
 
 import android.view.WindowManager
+import expo.modules.kotlin.functions.Queues
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class ExpoScreenshotGuardModule : Module() {
+  private val currentActivity
+    get() = appContext.currentActivity ?: throw ActivityNotFoundException()
+
   override fun definition() = ModuleDefinition {
     Name("ExpoScreenshotGuard")
 
     AsyncFunction("enableProtectionAsync") {
-      val activity = appContext.activityProvider?.currentActivity
-        ?: throw ActivityNotFoundException()
-      activity.runOnUiThread {
-        activity.window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
-      }
-    }
+      currentActivity.window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }.runOnQueue(Queues.MAIN)
 
     AsyncFunction("disableProtectionAsync") {
-      val activity = appContext.activityProvider?.currentActivity
-        ?: throw ActivityNotFoundException()
-      activity.runOnUiThread {
-        activity.window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
-      }
-    }
+      currentActivity.window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }.runOnQueue(Queues.MAIN)
 
     Function("isProtectionEnabled") {
-      val activity = appContext.activityProvider?.currentActivity
-        ?: return@Function false
+      val activity = appContext.currentActivity ?: return@Function false
       (activity.window.attributes.flags and WindowManager.LayoutParams.FLAG_SECURE) != 0
     }
   }

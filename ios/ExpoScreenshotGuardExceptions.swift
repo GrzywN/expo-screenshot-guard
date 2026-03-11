08d7cdb214c229c176fb90e394f9f0179c70c446
Karol Binkowski <karolbinkowski3@proton.me>
2026-03-11 11:27:37 +0100

    fix: add @unchecked Sendable

--- ios/ExpoScreenshotGuardExceptions.swift
+++ ios/ExpoScreenshotGuardExceptions.swift
@@ -1,6 +1,6 @@
 import ExpoModulesCore
 
-internal class WindowNotFoundException: Exception {
+internal class WindowNotFoundException: Exception, @unchecked Sendable {
   override var reason: String {
     "Window is not available"
   }
